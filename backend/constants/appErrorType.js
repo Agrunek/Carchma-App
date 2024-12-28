0bad8072530baa30bbc8bce51c16893573ef4c04
Adrian Antczak <adek24680@gmail.com>
2024-12-28 22:33:44 +0100

    [BE] Unify constant string values

--- backend/constants/appErrorType.js
+++ backend/constants/appErrorType.js
@@ -1,2 +1,2 @@
-export const UNCLASSIFIED = 'N/A';
-export const INVALID_ACCESS_TOKEN = 'InvalidAccessToken';
+export const UNCLASSIFIED = 'unclassified';
+export const INVALID_ACCESS_TOKEN = 'invalid_access_token';
