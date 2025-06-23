ddedd25428d74fff07c2a1e97d3c92da9786e9d9
Adrian Antczak <adek24680@gmail.com>
2025-06-24 00:28:07 +0200

    [FE] Migrate libs (src excluded)

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,41 +1,31 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import tailwind from 'eslint-plugin-tailwindcss'
-import eslintConfigPrettier from 'eslint-config-prettier'
-import pluginQuery from '@tanstack/eslint-plugin-query'
-import pluginRouter from '@tanstack/eslint-plugin-router'
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+import pluginRouter from '@tanstack/eslint-plugin-router';
+import pluginQuery from '@tanstack/eslint-plugin-query';
+import eslintConfigPrettier from 'eslint-config-prettier/flat';
 
-export default tseslint.config({
-  extends: [
-    js.configs.recommended,
-    ...tseslint.configs.recommended,
-    ...tailwind.configs['flat/recommended'],
-    ...pluginQuery.configs['flat/recommended'],
-    ...pluginRouter.configs['flat/recommended'],
-  ],
-  files: ['**/*.{ts,tsx}'],
-  ignores: ['dist'],
-  languageOptions: {
-    ecmaVersion: 2020,
-    globals: globals.browser,
-  },
-  plugins: {
-    'react-hooks': reactHooks,
-    'react-refresh': reactRefresh,
-  },
-  rules: {
-    ...reactHooks.configs.recommended.rules,
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
-  },
-  settings: {
-    tailwindcss: {
-      tags: ['tw'],
+export default tseslint.config(
+  { ignores: ['dist'] },
+  {
+    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+    plugins: {
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
     },
   },
-}, eslintConfigPrettier)
+  ...pluginRouter.configs['flat/recommended'],
+  ...pluginQuery.configs['flat/recommended'],
+  eslintConfigPrettier,
+);
