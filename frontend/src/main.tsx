0d983cd8c8c7dc273a5bb52b5f895dc0e73f5b6d
Adrian Antczak <adek24680@gmail.com>
2025-06-24 00:28:07 +0200

    [FE] Migrate libs (src excluded)

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import reportWebVitals from './reportWebVitals.ts';
 import App from './App.tsx';
 import './index.css';
 
@@ -8,3 +9,5 @@ createRoot(document.getElementById('root')!).render(
     <App />
   </StrictMode>,
 );
+
+reportWebVitals();
