503b79f43faf757b844e9895745696c621ca6473
Adrian Antczak <adek24680@gmail.com>
2025-07-10 23:50:56 +0200

    [FE] Add auth to user global context

--- frontend/src/types/context.ts
+++ frontend/src/types/context.ts
@@ -1,5 +1,19 @@
 import type { QueryClient } from '@tanstack/react-query';
+import type { GetCurrentProfileResult } from '@/types/api';
+
+interface AuthContextLogged {
+  isAuthenticated: true;
+  user: GetCurrentProfileResult;
+}
+
+interface AuthContextFailed {
+  isAuthenticated: false;
+  user: null;
+}
+
+export type AuthContext = AuthContextLogged | AuthContextFailed;
 
 export interface GlobalRouterContext {
   queryClient: QueryClient;
+  auth: AuthContext;
 }
