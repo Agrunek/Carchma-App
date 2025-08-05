9f93a6ab8c7ce76395c7ff48c6e72c44b0921eef
Adrian Antczak <adek24680@gmail.com>
2025-08-05 23:23:59 +0200

    [FE] Update route tree

--- frontend/src/routes/register/route.tsx
+++
@@ -1,9 +0,0 @@
-import { createFileRoute, redirect } from '@tanstack/react-router';
-
-export const Route = createFileRoute('/register')({
-  beforeLoad: ({ context }) => {
-    if (context.auth.isAuthenticated) {
-      throw redirect({ to: '/' });
-    }
-  },
-});
