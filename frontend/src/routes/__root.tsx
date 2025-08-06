615f97e20e8b1663d4d12043b8c4b9d5053f13b6
Adrian Antczak <adek24680@gmail.com>
2025-08-06 23:28:02 +0200

    [FE] Clear components folder

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -1,3 +1,4 @@
+import type { ErrorComponentProps } from '@tanstack/react-router';
 import type { GlobalRouterContext } from '@/types/context';
 
 import { createRootRouteWithContext, Outlet } from '@tanstack/react-router';
@@ -5,10 +6,12 @@ import { TanStackRouterDevtools } from '@tanstack/react-router-devtools';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 export const Route = createRootRouteWithContext<GlobalRouterContext>()({
-  component: Component,
+  component: RouteComponent,
+  errorComponent: ErrorComponent,
+  notFoundComponent: NotFoundComponent,
 });
 
-function Component() {
+function RouteComponent() {
   return (
     <>
       <Outlet />
@@ -16,4 +19,12 @@ function Component() {
       <ReactQueryDevtools />
     </>
   );
+}
+
+function ErrorComponent({ error }: ErrorComponentProps) {
+  return <div>{error.message}</div>;
+}
+
+function NotFoundComponent() {
+  return <div>Hello "/???"!</div>;
 }
