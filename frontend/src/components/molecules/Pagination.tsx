79c299722b73e142edad0f5017a803873d743bf8
Adrian Antczak <adek24680@gmail.com>
2025-12-26 20:05:44 +0100

    [FE] Christmas package update

--- frontend/src/components/molecules/Pagination.tsx
+++ frontend/src/components/molecules/Pagination.tsx
@@ -27,7 +27,7 @@ const Pagination = ({ className, currentPage, totalPages, onChange }: Pagination
 
         return (
           <Button
-            className={n === currentPage ? 'font-black!' : ''}
+            className={n === currentPage ? 'scale-110 font-black!' : ''}
             key={n}
             onClick={() => onChange(n)}
             variant={isEllipsis ? 'tertiary' : 'secondary'}
