d262890768c97812d68fc5c3a2bc0551f4cf8543
Adrian Antczak <adek24680@gmail.com>
2025-12-31 01:50:22 +0100

    [FE] Add theme switching logic with local storage

---
+++ frontend/src/utils/theme.ts
@@ -0,0 +1,20 @@
+type Theme = 'light' | 'dark' | 'system';
+
+export const setTheme = (theme: Theme) => {
+  switch (theme) {
+    case 'light':
+      localStorage.theme = 'light';
+      break;
+    case 'dark':
+      localStorage.theme = 'dark';
+      break;
+    case 'system':
+      localStorage.removeItem('theme');
+  }
+
+  document.documentElement.classList.toggle(
+    'dark',
+    localStorage.theme === 'dark' ||
+      (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches),
+  );
+};
