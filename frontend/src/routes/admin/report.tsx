92c7dd09f363715798ad57be1168348df07b01bf
Adrian Antczak <adek24680@gmail.com>
2026-02-03 03:20:46 +0100

    [FE] Add comments

--- frontend/src/routes/admin/report.tsx
+++ frontend/src/routes/admin/report.tsx
@@ -1,5 +1,14 @@
-import { createFileRoute } from '@tanstack/react-router';
+import { createFileRoute, redirect } from '@tanstack/react-router';
 
 export const Route = createFileRoute('/admin/report')({
+  beforeLoad: ({ context, location }) => {
+    if (!context.auth.isAuthenticated) {
+      throw redirect({ to: '/auth/login', search: { redirect: location.href } });
+    }
+
+    if (!context.auth.user.permissions.includes('report_reviewer')) {
+      throw redirect({ to: '/' });
+    }
+  },
   component: () => <div>Hello "/admin/report"!</div>,
 });
